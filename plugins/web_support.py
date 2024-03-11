fbe2b02900deeb9486df10d252e9cc2a0931c5e3
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-03-11 12:05:28 +0530

    Update web_support.py

--- plugins/web_support.py
+++ plugins/web_support.py
@@ -5,7 +5,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("https://t.me/RenameSnowProBot")
+    return web.json_response("https://t.me/codeflix_bots")
 
 
 async def web_server():
